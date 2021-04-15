1b45ab50fd0d579ed7c829ad380cf0b60ac28aac
Justin Wall <walljd20@gmail.com>
2021-04-15 18:44:39 -0500

    course notes 04/15/2021

---
+++ udemy_Plotly-Dashboards-with-Dash/2-09-MultipleOutputs/callbackmultout.py
@@ -0,0 +1,36 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\wallj\DS_Projects\\Plotly-Dashboards-with-Dash\\Data\\wheels.csv')
+
+app = dash.Dash()
+
+app.layout = html.Div([
+            dcc.RadioItems(id='wheels',
+                            options=[{'label':i,'value':i} for i in df['wheels'].unique()],
+                            value=1
+                            ),
+            html.Div(id='wheels-output'),
+            html.Hr(),
+            dcc.RadioItems(id='colors',
+                            options=[{'label':i,'value':i} for i in df['color'].unique()],
+                            value='blue'
+                            ),
+            html.Div(id='colors-output')
+],style={'fontFamily':'helvetica','fontSize':18})
+
+@app.callback(Output('wheels-output','children'),
+             [Input('wheels','value')])
+def callback_a(wheels_value):
+    return "you chose {}".format(wheels_value)
+
+@app.callback(Output('colors-output','children'),
+             [Input('colors','value')])
+def callback_b(colors_value):
+    return "you chose {}".format(colors_value)
+
+if __name__ == '__main__':
+    app.run_server()
